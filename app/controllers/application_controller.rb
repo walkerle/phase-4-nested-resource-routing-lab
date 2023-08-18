c967c92abb74a8e7729b02d961bb237fcc0376fc
Walker Le <walker.le88@gmail.com>
2023-08-17 22:13:06 -0600

    lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+  def render_not_found_response exception
+    render json: {error: "#{exception.model} not found"}, status: :not_found
+  end
+
+  # def record_invalid exception
+  #   render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  # end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id] # :user_id is added to the params hash thru the nested routes resources
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find params[:user_id]
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
