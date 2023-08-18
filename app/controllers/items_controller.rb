19c02be5dda0a01d4bd3e711ea83b27fa1035173
Walker Le <walker.le88@gmail.com>
2023-08-17 18:55:11 -0600

    bootcamp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if(params[:user_id])
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user, status: :ok
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def render_not_found
+    render json: { error: "User Not Found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
